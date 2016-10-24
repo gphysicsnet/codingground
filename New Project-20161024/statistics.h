5a438a04c7aa9a08db0a36e475d44c3d66025b1e
gphysicsnet <wgerber@gphysics.net>
2016-10-24 18:14:14 -0300

    Created by Coding Ground Client

---
+++ New Project-20161024/statistics.h
@@ -0,0 +1,10 @@
+class statistics {
+  
+public:
+	void resetear();
+	int agregar(double valor);
+	void analizar();
+	double getValor(int idx);
+	// constructor
+	statistics(int numero,double desde, double hasta);
+};
